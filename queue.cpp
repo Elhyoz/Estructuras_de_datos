aed349cef8c69d54cda69f31d521e8c72b315b83
Baruch Guerra <dolce_dmn@hotmail.com>
2017-08-16 02:33:13 -0500

    Fixed some bugs in the switch selection menu

--- queue.cpp
+++ queue.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 typedef int TipoDato;
 typedef struct Queue{
-	TipoDato fila[TAM];
+	int fila[TAM];
 	int front, rear;
 }queue;
 
@@ -20,12 +20,59 @@ void menu();
 void print(queue *);
 
 int main(void){
-	menu();
+	queue q1;
+	cout<<q1.fila;
+	int element, dequeued;
+	initialize(&q1);
 	int option;
-	switch(option){
-		case 1:
-			if()
-	}
+	fflush(stdin);
+	do{
+		menu();
+		cin>>option;
+		switch(option){
+			
+			case 1:
+				if(!full(&q1)){
+					cout<<"Introduzca un elemento"<<endl;
+					cin>>element;
+					enqueue(&q1, element);
+				}
+				else if(full(&q1)){
+					cout<<"Fila llena"<<endl;
+					system("pause");
+				}
+				break;
+				
+			case 2:
+				if(!empty(&q1)){
+					dequeued = dequeue(&q1);
+					cout<<dequeued;
+					cout<<"Sale:"<<" "<<dequeued<<endl;
+					system("pause");
+				}
+				else if(empty(&q1)){
+					cout<<"Fila vacia"<<endl;
+					system("pause");
+				}
+				break;
+				
+			case 3:
+				if(!empty(&q1)){
+					print(&q1);
+					system("pause");
+				}
+				else
+					cout<<"Fila vacia"<<endl;
+					system("pause");
+				break;
+			
+			case 4:
+				break;
+			
+			default: 
+				cout<<"Opcion no valida..."<<endl;
+		}
+	}while(option!=4);
 	
 	return 0;
 }
@@ -39,20 +86,29 @@ int full(queue *pq){
 }
 
 int empty(queue *pq){
-	return pq->front=pq->rear?1:0;
+	if(pq->front==pq->rear){
+		return 1;
+	}
+	else
+		return 0;
+//	return pq->front=pq->rear?1:0;
 }
 
 void enqueue(queue *pq, int element){
+	cout<<"Ingresaste: "<<element<<endl;
+	system("pause");
 	pq->fila[pq->rear]=element;
-	(pq->rear)++;
+	pq->rear++;
 }
 
 int dequeue(queue *pq){
 	int temporal = pq->fila[pq->front];
-	for(int i=0;i<pq->rear-1;i++){
+	cout<<temporal<<endl;
+	system("pause");
+	for(int i = 0;i < pq->rear-1;i++){
 		pq->fila[i] = pq->fila[i+1];
 	}
-	(pq->rear)--;
+	pq->rear--;
 	return temporal;
 }
 
@@ -61,11 +117,11 @@ void initialize(queue *pq){
 	pq->rear = 0;
 }
 
-void menu(void){
+void menu(){
 	system("cls");
 	cout<<"Menu. Fila con arreglo estatico"<<endl<<endl;
 	cout<<"Opcion 1: Enqueue \n\nOpcion 2: Dequeue \n\nOpcion 3: Imprimir \n\nOpcion 4: Salir"<<endl;
-	cout<<"Opcion: "<<endl;
+	cout<<"Opcion: ";
 }
 
 void print(queue *pq){
