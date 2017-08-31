8a45c98dd567aa98267506b4ba13b7a15e8d1cf0
Baruch-Guerra <dolce_dmn@hotmail.com>
2017-08-31 01:25:05 -0500

    Finished the basic functionality of the program

--- filas/random_queue.cpp
+++ filas/random_queue.cpp
@@ -6,54 +6,69 @@
 #define TAM 10
 using namespace std;
 
+/* TODO (#1#): AÃ±adir la funcionalidad con el menu y la
+		utilizacion de las demas funciones */
 
-/* TODO (#1#): Añadir la funcionalidad con el menú y la 
-		utilización de las demás funciones */
-		
 
-typedef struct Random{
+struct Random{
 	int queue[TAM];
 	int front, rear;
-}random;
+};
 
-int full(random *);
-void fill(random *);
-int empty(random *);
-void enqueue(random *, int); /* (fila, elemento a insertar) */
-int dequeue(random *);
-void initialize(random *);
-void random_fill(random *);
-void print(random *);
+int full(Random *);
+void fill(Random *);
+int empty(Random *);
+void enqueue(Random *, int); /* (fila, elemento a insertar) */
+int dequeue(Random *);
+void initialize(Random *);
+void random_fill(Random *);
+void print(Random *);
 void menu();
 
 int main(void){
-	random q1, par, impar;
+	Random q1;
+	Random par;
+	Random impar;
+
 	srand(time(NULL));
 	int fila_random[TAM];
 	int dequeued, option;
 	int random_numbers = rand()%10;
 	initialize(&q1);
 	random_fill(&q1);
+	cout<<"Fila rellenada de manera aleatoria: "<<setw(2);
 	for(int i=0;i<TAM;i++){
-		cout<<q1.queue[i]<<endl;
+		cout<<q1.queue[i]<<", ";
 	}
 	fflush(stdin);
-	dequeued = dequeue(&q1);
-	if(dequeued%2 == 0){
-		enqueue(&par, dequeued);
+
+	for(int i=0; i<TAM; i++){
+		dequeued = dequeue(&q1);
+		if(dequeued%2 == 0){
+			enqueue(&par, dequeued);
+		}
+		else
+			enqueue(&impar, dequeued);
 	}
-	else
-		enqueue(&impar, dequeued);
-	
+
+	cout<<"Fila de pares"<<endl;
+	for(int i = 0;i < TAM; i++){
+        cout<<par.queue[i]<<", ";
+	}
+
+	cout<<"Fila de impares"<<endl;
+	for(int i = 0;i < TAM; i++){
+        cout<<impar.queue[i]<<", ";
+	}
 }
 
-void random_fill(random *pq){
+void random_fill(Random *pq){
 	for(int i=0; i<TAM; i++){
 		pq->queue[i] = rand()%10;
 	}
 }
 
-void initialize(random *pq){
+void initialize(Random *pq){
 	pq->front = 0;
 	pq->rear = 0;
 }
@@ -65,7 +80,7 @@ void menu(){
 	cout<<"Opcion: ";
 }
 
-int dequeue(random *pq){
+int dequeue(Random *pq){
 	int temporal = pq->queue[pq->front];
 	for(int i = 0;i < pq->rear-1;i++){
 		pq->queue[i] = pq->queue[i+1];
@@ -74,14 +89,15 @@ int dequeue(random *pq){
 	return temporal;
 }
 
-void enqueue(random *pq, int dequeued){
+void enqueue(Random *pq, int dequeued){
 	cout<<"Ingreso: "<<dequeued<<endl;
-	system("pause");
+	cout<<"Presione una tecla para continuar..."<<endl;
+	getchar();
 	pq->queue[pq->rear]=dequeued;
 	pq->rear++;
 }
 
-int empty(random *pq){
+int empty(Random *pq){
 	if(pq->front==pq->rear){
 		return 1;
 	}
@@ -90,7 +106,7 @@ int empty(random *pq){
 //	return pq->front=pq->rear?1:0;
 }
 
-int full(random *pq){
+int full(Random *pq){
 	if(pq->rear == TAM){
 		return 1;
 	}
